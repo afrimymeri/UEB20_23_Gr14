fa2c54d1ea972ace80e34b8bf4b2b91f2e864e31
afrimymeri <afrimymeri0@gmail.com>
2024-01-03 02:42:46 +0100

    jQueryslide

--- script/jquery.js
+++ script/jquery.js
@@ -1,30 +1,4 @@
-$(document).ready(function(){
-    $(".hide").click(function(){
-      $(this).hide();
-    });
-  });
-
-
-  $(document).ready(function(){
-    // Hide the table initially
-    // $("table").hide();
-
-    // Button click event to hide the table
-    $("#hideBtn").click(function(){
-      $("table").hide();
-    });
-
-    // Button click event to show the table
-
-  });    
-  
-  $(document).ready(function(){
-    // $("table").show();
-
-    $("#showBtn").click(function(){
-        $("table").show();
-      });
-
-  });
-  
-  
+$(document).ready(function () {
+    // Slide in the button when the document is ready
+    $("#buttonFadein").slideDown();
+});
--- script/main.js
+++ script/main.js
@@ -1,19 +1,57 @@
-// Get the header element
-var header = document.getElementById("sticky-header");
+var width=500;
+		var difference=2;
+		var interveralID =0;
+		//document.getElementById("img1").style.width=width;
 
-// Get the offset position of the header
-var sticky = header.offsetTop;
+		function increase()
+		{
+			clearInterval(interveralID);
+			interveralID=setInterval(expand,10);
+		}
+		function decrease()
+		{
+			clearInterval(interveralID);
+			interveralID=setInterval(shrink,10);
+		}
+		function expand()
+		{
+			if(width<500)
+			{
+				width = width+difference;
+				document.getElementById("foto1").style.width=width;
+				console.log(width);
+			}
+			else
+			{
+				clearInterval(interveralID);
+			}
+			
+		}
+		function shrink()
+		{
+			if(width>100)
+			{
+				width = width-difference;
+				document.getElementById("foto1").style.width=width;
+				console.log(width);
+			}
+			else
+			{
+				clearInterval(interveralID);
+			}
+			
+		}
 
-// Function to add the "sticky" class to the header when scrolling down
-function stickyHeader() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
 
-// Attach the function to the scroll event
-window.onscroll = function() {
-  stickyHeader();
-};
+    console.log("A O KA BON QIKY MUT");
+
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+      var fadeInButton = document.getElementById("buttonFadein");
+  
+      // Trigger the fade-in effect by adding a class after a short delay
+      setTimeout(function () {
+          fadeInButton.classList.add("fade-in");
+      }, 100); // You can adjust the delay as needed
+  });
