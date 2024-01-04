199e59f762753dc2e54c5e7a69c748ee2a82cc7e
afrimymeri <afrimymeri0@gmail.com>
2024-01-04 19:53:00 +0100

    ndrysh

--- script/main.js
+++ script/main.js
@@ -1,57 +1,13 @@
-var width=500;
-		var difference=2;
-		var interveralID =0;
-		//document.getElementById("img1").style.width=width;
-
-		function increase()
-		{
-			clearInterval(interveralID);
-			interveralID=setInterval(expand,10);
-		}
-		function decrease()
-		{
-			clearInterval(interveralID);
-			interveralID=setInterval(shrink,10);
-		}
-		function expand()
-		{
-			if(width<500)
-			{
-				width = width+difference;
-				document.getElementById("foto1").style.width=width;
-				console.log(width);
-			}
-			else
-			{
-				clearInterval(interveralID);
-			}
-			
-		}
-		function shrink()
-		{
-			if(width>100)
-			{
-				width = width-difference;
-				document.getElementById("foto1").style.width=width;
-				console.log(width);
-			}
-			else
-			{
-				clearInterval(interveralID);
-			}
-			
-		}
-
-
-    console.log("A O KA BON QIKY MUT");
-
-
-
-    document.addEventListener("DOMContentLoaded", function () {
-      var fadeInButton = document.getElementById("buttonFadein");
+function allowDrop(ev) {
+	ev.preventDefault();
+  }
   
-      // Trigger the fade-in effect by adding a class after a short delay
-      setTimeout(function () {
-          fadeInButton.classList.add("fade-in");
-      }, 100); // You can adjust the delay as needed
-  });
+  function drag(ev) {
+	ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(data));
+  }
